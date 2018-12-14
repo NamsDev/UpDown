0f3030e2a083a92a1a35bb3b890c790538bd10ac
Romain Grosse <v-rogro@microsoft.com>
2018-12-14 14:03:25 +0100

    center plateform

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public bool isReversed;
+    public float maxSpeed = 1;
+
+    private Rigidbody2D rigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float input = Input.GetAxis("Horizontal");
+        input *= isReversed ? -1 : 1;
+
+        Debug.Log(input);
+
+        rigidbody.velocity = new Vector2(input * maxSpeed, rigidbody.velocity.y);
+	}
+}
---
+++ Assets/centerPlateform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class centerPlateform : MonoBehaviour {
+
+	public Transform yin;
+	public Transform yan;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = (yin.position + yan.position) / 2;
+	}
+}
