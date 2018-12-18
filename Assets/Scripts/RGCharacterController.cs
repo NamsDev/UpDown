141741d1665ba31d2f70aaefffdd448a58895633
Romain GROSSE <grosseromain@hotmail.fr>
2018-12-19 00:29:48 +0100

    to test

--- Assets/Scripts/RGCharacterController.cs
+++ Assets/Scripts/RGCharacterController.cs
@@ -1,49 +1,60 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class RGCharacterController : MonoBehaviour
 {
-    public float maxSpeed = 3;
-    public float acceleration = 1;
-    public float jumpForce = 1;
+	public float maxSpeed = 3;
+	public float acceleration = 1;
+	public float jumpForce = 1;
 	public bool isReversed;
 
-    public Transform groundCheck;
-    public float radius;
-    public LayerMask whatIsGround;
+	public Transform groundCheck;
+	public float radius;
+	public LayerMask whatIsGround;
 
-    private Animator animator;
+	private Animator animator;
 	private Rigidbody2D rigidbody;
 	private bool isFacingRight = true;
 
 	// Use this for initialization
 	void Start()
 	{
-        rigidbody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-    }
-	
+		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+
+	void FixedUpdate()
+	{
+		Debug.Log(rigidbody.velocity + "   FU");
+	}
+
 	// Update is called once per frame
 	void Update()
 	{
-		if ( (rigidbody.velocity.x < 0 && isFacingRight) || (rigidbody.velocity.x > 0 && !isFacingRight))
+		Debug.Log(rigidbody.velocity + "   U");
+
+		if ((rigidbody.velocity.x < 0 && isFacingRight) || (rigidbody.velocity.x > 0 && !isFacingRight))
 		{
 			SwapSide();
 		}
-		//animator.SetFloat("moveSpeed", Mathf.Abs(rigidbody.velocity.x)); TODO ask
+		animator.SetFloat("xSpeed", Mathf.Abs(rigidbody.velocity.x));
+		animator.SetFloat("ySpeed", rigidbody.velocity.y * (isReversed ? -1 : 1));
 	}
 
-    public void Jump()
-    {
-	    rigidbody.AddForce(jumpForce * Vector2.up * (isReversed ? -1 : 1), ForceMode2D.Impulse);
-    }
+	public void Jump()
+	{
+		rigidbody.AddForce(jumpForce * Vector2.up * (isReversed ? -1 : 1), ForceMode2D.Impulse);
+	}
 
-    public void Move(float input) //ratio is [-1:1]
-    {
-        if (Mathf.Abs(rigidbody.velocity.x) < maxSpeed * Mathf.Abs(input))
-        {
-            rigidbody.AddForce(input * acceleration * Vector2.right);
-        }
+	public void Move(float input) //ratio is [-1:1]
+	{
+		//if (Mathf.Abs(rigidbody.velocity.x) < maxSpeed * Mathf.Abs(input))
+		{
+			rigidbody.AddForce(input * acceleration * Vector2.right);
+		}
 
+		Debug.Log(rigidbody.velocity + "   SU");
 
 	}
 
@@ -51,6 +62,12 @@ public class RGCharacterController : MonoBehaviour
 	{
 		transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
 		isFacingRight = !isFacingRight;
-    }
+	}
 
+
+	void OnDrawGizmosSelected()
+	{
+		Handles.color = Color.green;
+		Handles.DrawWireDisc(groundCheck.position, groundCheck.transform.forward, radius);
+	}
 }
